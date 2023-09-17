2723c549232ecd81ac567dc6b1d51f9a41a67461
tosaken1116 <nagato20021122@gmail.com>
2023-09-17 16:46:00 +0900

    chore: add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import { COLORS } from './src/constants/colors';
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,7 +13,39 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'deep-red': {
+          main: COLORS.DEEP_RED.MAIN,
+          light: COLORS.DEEP_RED.LIGHT,
+          dark: COLORS.DEEP_RED.DARK,
+        },
+        'gold': {
+          main: COLORS.GOLD.MAIN,
+          light: COLORS.GOLD.LIGHT,
+          dark: COLORS.GOLD.DARK,
+        },
+        'custom-dark': {
+          main: COLORS.BLACK.MAIN,
+          light: COLORS.BLACK.LIGHT,
+          dark: COLORS.BLACK.DARK,
+        },
+        'success': {
+          main: COLORS.SUCCESS.MAIN,
+          light: COLORS.SUCCESS.LIGHT,
+          dark: COLORS.SUCCESS.DARK,
+        },
+        'error': {
+          main: COLORS.ERROR.MAIN,
+          light: COLORS.ERROR.LIGHT,
+          dark: COLORS.ERROR.DARK,
+        },
+        'info': {
+          main: COLORS.INFO.MAIN,
+          light: COLORS.INFO.LIGHT,
+          dark: COLORS.INFO.DARK,
+        },
+      },
     },
   },
   plugins: [],
-}
+};
