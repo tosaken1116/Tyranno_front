c2faeb6b58e3d8e97bfaa8a04f1594d309fbc66f
tosaken1116 <nagato20021122@gmail.com>
2023-09-20 01:52:18 +0900

    fix: progress number uiの修正

--- src/components/ui/ProgressNumber/index.tsx
+++ src/components/ui/ProgressNumber/index.tsx
@@ -1,5 +1,7 @@
 import type { HTMLAttributes } from 'react';
 
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import clsx from 'clsx';
 
 import { Stack } from '../Stack';
@@ -23,18 +25,27 @@ export const ProgressNumber: React.FC<Props> = ({
   const progressNumberStyle = clsx(defaultStyle, className);
 
   return (
-    <Stack direction="row" spacing={4}>
+    <Stack direction="row" spacing={4} className="items-center">
       {Array.from({ length: totalNum }).map((_, i) => (
-        <p
-          key={i}
-          style={{
-            backgroundColor:
-              i + 1 == selectedNum ? COLORS.DEEP_RED.MAIN : COLORS.BLACK.MAIN,
-          }}
-          className={progressNumberStyle}
-        >
-          {i + 1}
-        </p>
+        <>
+          <p
+            key={i}
+            style={{
+              backgroundColor:
+                i + 1 == selectedNum ? COLORS.DEEP_RED.MAIN : COLORS.BLACK.MAIN,
+            }}
+            className={progressNumberStyle}
+          >
+            {i + 1}
+          </p>
+          {i + 1 != totalNum && (
+            <FontAwesomeIcon
+              icon={faArrowRight}
+              size="xl"
+              className="text-custom-dark-light"
+            />
+          )}
+        </>
       ))}
     </Stack>
   );
