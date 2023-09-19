1c55a7b6dd6ab140f2244ef204b8663cbc21a191
tosaken1116 <nagato20021122@gmail.com>
2023-09-20 01:44:16 +0900

    feat: containerの追加

---
+++ src/components/model/CreateAccountCard/index.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { useCreateAccountCard } from './hooks';
+import { CreateAccountCardPresentation } from './presentations';
+import { CreateAccountCardErrorPresentation } from './presentations/error';
+import { CreateAccountCardLoadingPresentation } from './presentations/loading';
+
+import { ProgressNumber } from '@/components/ui/ProgressNumber';
+
+export const CreateAccountCard: React.FC = () => {
+  const {
+    handleCodeChange,
+    handlePhoneNumberChange,
+    phoneNumberValue,
+    verifyCode,
+    sendSMSCode,
+    signInWithGoogle,
+    authProgress,
+    totalProgress,
+    isLoading,
+    errorText,
+  } = useCreateAccountCard();
+  return (
+    <div className="bg-gold-dark relative flex flex-col items-center rounded-large p-8 w-[640px]">
+      <div className="content-center">
+        <ProgressNumber totalNum={totalProgress} selectedNum={authProgress} />
+      </div>
+      <CreateAccountCardPresentation
+        handleCodeChange={handleCodeChange}
+        handlePhoneNumberChange={handlePhoneNumberChange}
+        phoneNumberValue={phoneNumberValue}
+        verifyCode={verifyCode}
+        sendSMSCode={sendSMSCode}
+        signInWithGoogle={signInWithGoogle}
+        authProgress={authProgress}
+      />
+      {errorText && (
+        <>
+          <div className="bg-[#00000033] w-[640px] h-[480px] absolute top-0 z-0" />
+          <div className="absolute top-1/2 z-10">
+            <CreateAccountCardErrorPresentation errorText={errorText} />
+          </div>
+        </>
+      )}
+      {isLoading && (
+        <>
+          <div className="bg-[#00000033] w-[640px] h-[480px] absolute top-0 z-0" />
+          <div className="absolute top-1/2 z-100">
+            <CreateAccountCardLoadingPresentation />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
